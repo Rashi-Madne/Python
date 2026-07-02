01d9277e7081c325e229715fb01e222915a49f61
Rashi <rashimadne13@gmail.com>
2026-07-02 09:55:26 +0530

    Complete Chapter 3: Operators and Expressions

---
+++ chatpter3.py
@@ -0,0 +1,47 @@
+#q1
+a=25
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**2)
+
+#q2
+score=50
+print(score)
+score+=2
+print(score)
+score-=2
+print(score)
+score*=2
+print(score)
+
+#q3
+print(10>5)
+print(10==5)
+print(20!=20)
+print(8<=10)
+print(" ")
+#q4
+print(True and False)
+print(True or False)
+print(not False)
+
+print(" ")
+
+#q5
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(a == b)
+print(a is b)
+
+print(" ")
+
+#q6
+print(5>2 and 8>3)
+print(5 > 2 and 8 < 3)
+print(5 < 2 or 8 > 3)
+print(not (5 > 2))
