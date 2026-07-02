3aeb96868271db069f41d6ad88b7190bb6aa54b8
Rashi <rashimadne13@gmail.com>
2026-07-02 13:19:54 +0530

    Complete Chapter 5: Introduction to Loops

---
+++ chapter5.py
@@ -0,0 +1,28 @@
+for i in range(5):
+    print("Radhe Radhe")
+
+#q1
+for i in range(7):
+    print("Bangtan")
+
+#q2
+for i in range(3):
+    print(i)
+
+for i in range(1,6):
+    print(i)
+
+#q3
+for i in range(10,16):
+    print(i)
+
+#q4
+for i in range(5,31,5):
+    print(i)
+
+for i in range(5,0,-1):
+    print(i)
+
+#q5
+for i in range(10,4,-1):
+    print(i)
