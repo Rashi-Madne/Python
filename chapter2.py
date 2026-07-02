9ebc6d880397e9db4612b05a5a448fe37d5ef072
Rashi <rashimadne13@gmail.com>
2026-07-02 08:32:43 +0530

    Complete Chapter 2: Variables and Data Types

---
+++ chapter2.py
@@ -0,0 +1,32 @@
+#q1
+name="harry"
+age=5
+height=6.0
+is_student=True
+print(name)
+print(age)
+print(height)
+print(is_student)
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+#q2
+name=input("name - ")
+age=input('age : ')
+age=int(age)
+lang=input("fav programming language: ")
+print("hello",name,'!')
+print("you are ",age,"years old")
+print(f"your fav lang is {lang}")
+print(type(age))
+
+num1='50'
+num2="25"
+num1=int(num1)
+num2=int(num2)
+print("addition is",num1+num2)
+print(f"subtraction is {num1-num2}")
+print(f"multiplication is {num1*num2}")
+print("division is",num1/num2)
