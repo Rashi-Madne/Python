2516ae8c201167c6a584ad59e3e1786d1a5c07c8
Rashi <rashimadne13@gmail.com>
2026-07-02 18:19:10 +0530

    Complete Chapter 6: While loops and loop control

---
+++ chapter6.py
@@ -0,0 +1,106 @@
+count =1
+while count<=5:
+    print(count)
+    count+=1
+
+print(" ")
+
+#q1
+count=1
+while count<8:
+    print("hello")
+    count+=1
+
+print(" ")
+
+x=4
+while x>0:
+    print(x)
+    x-=1
+
+print(" ")
+#break
+
+for i in range(1,11):
+    if i==6:
+        break
+    print(i)
+
+print(" ")
+
+for i in range(1,11):
+    if i==8:
+        print("found")
+        break        
+    print(i)
+
+print(" ")
+
+for i in range(1,20):
+    if i==6:
+        break
+    print(i)
+
+print(" ")
+
+#q2
+for i in range(5):
+    print(i)
+    if i==2:
+        break
+
+print(" ")
+#continue 
+
+for i in range(1,6):
+    if i==3:
+        continue
+    print(i)
+
+print(" ")
+
+#q3
+for i in range(1,8):
+    if i==4:
+        continue
+    print(i)
+
+print(" ")
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
