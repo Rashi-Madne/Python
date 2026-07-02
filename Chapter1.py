697d26e7e1eaac8aa4730a3c1337f026dd34cef0
Rashi <rashimadne13@gmail.com>
2026-07-02 08:12:48 +0530

    Complete Lesson 1: Python basics

---
+++ Chapter1.py
@@ -0,0 +1,21 @@
+# Exercise 1
+name="Rashi"
+age=20
+city='Nagpur'
+print(f"my name is {name}")
+print(f"My age is {age}")
+print(f"I live in {city}")
+
+#Exercise 2
+a=10
+b=5
+print(f"addition of {a} and {b} is {a+b}")
+print(f"subtraction of {a} and {b} is {a-b}")
+print(f"multiplication of {a} and {b} is {a*b}")
+print(f"division of {a} and {b} is {a/b}")
+
+#exercise 3
+a=input("Your name: ")
+b=int(input("your age :"))
+print(f"hello {a}, you are {b} years old")
+
