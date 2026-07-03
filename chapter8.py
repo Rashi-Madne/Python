062cc430208cf7fada0969c3f10b841bda4dc891
Rashi <rashimadne13@gmail.com>
2026-07-03 18:28:40 +0530

    Complete Chapter 8: Strings

--- chapter8.py
+++ chapter8.py
@@ -6,10 +6,14 @@ print(city[0])
 print(city[2])
 print(city[5])
 
+print()
+
 #negative indexing
 
 word="python"
 print(word[-1])
+
+print()
 
 #q2
 city="Nagpur"
@@ -17,16 +21,22 @@ print(city[-1])
 print(city[-2])
 print(city[-3])
 
+print()
+
 #q3
 text="hello"
 print(text[-1])
 print(text[-5])
 
-#slicing
+print()
 
+#slicing
+ 
 word="python"
 print(word[0:3])
 print(word[2:5])
+
+print()
 
 #q4
 fruit="Watermelon"
@@ -47,7 +57,9 @@ print(name[:])
 print()
 
 name="Rashi"
-print(name[::-1])
+print(name[::-1])  #reverse string
+
+print()
 
 #q5
 city="Nagpur"
@@ -63,6 +75,8 @@ name="rashi"
 print(name.upper()) #upper doesnt change original string
 name="HARRY"
 print(name.lower())
+
+print()
 
 #q6
 movie = "INTERSTELLAR"
@@ -95,6 +109,8 @@ print()
 sentence="I like java"
 print(sentence.replace("java","python"))
 
+print()
+
 #q9
 sen="I like cats"
 print(sen.replace("cats","dogs"))
@@ -114,7 +130,124 @@ word="banana"
 print(word.count("a"))
 print(word.count("n"))
 
+print()
+
 #q10
 text="Missisippi"
 print(text.count("s"))
 print(text.count("i"))
+
+print()
+
+#string immutability
+
+name="Carry"
+name="Harry"
+print(name)
+
+print()
+
+#q11
+city="NGP"
+new_city="Nagpur"
+print(city)
+print(new_city)
+
+print()
+
+word="bananaS"
+print(word.replace("S","s"))
+print(word)
+
+print()
+
+print(word.replace("a","o"))
+
+print()
+
+word=word.replace("b","B")
+print(word)
+
+print()
+
+word=word.upper()
+print(word)
+
+print()
+
+print(word.upper())
+
+print()
+
+#Escape character
+print("he said \"hello\"")  #\tells next char has special meaning
+
+print()
+
+print("hello\nworld")
+
+print()
+
+print("name\tage")
+print("harry\t5")
+
+print()
+
+print("\\")  #to print backlash
+
+print()
+
+#q12
+print("my name is \"Rashi\"\n\nI love Python")
+
+print()
+print("Hello\nPython\tWorld")
+
+print()
+text = "Python"
+print(text.upper().replace("P","J").lower())
+
+print()
+#built-in functions
+
+name="Rashi"
+a=len(name)
+print(name)
+print(a)
+
+print()
+
+#q13
+city="Nagpur"
+print(len(city))
+
+print()
+
+print(min("apple"))
+print(max("Harry"))
+
+print()
+
+print(sorted("Harry"))
+print(sorted("rashi"))
+print(sorted("Rudra"))
+
+print()
+
+text="Python"
+print("P"in text)
+print("Z"in text)
+
+print()
+
+sentence="I love python"
+print("love"not in sentence)
+
+print()
+
+#q14
+text="Programming"
+print("gram" in text)
+print("Gram"in text)
+
+print()
